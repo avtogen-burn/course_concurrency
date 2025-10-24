2133816364c077f2be395664adeaef6b058684e3
avtogen-burn <avtogen_fire@mail.ru>
2025-10-24 15:42:34 +0300

    async price aggregator

--- src/main/java/course/concurrency/m2_async/minPrice/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/minPrice/PriceAggregator.java
@@ -1,11 +1,18 @@
 package course.concurrency.m2_async.minPrice;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PriceAggregator {
 
     private PriceRetriever priceRetriever = new PriceRetriever();
+
+    private ExecutorService executor = Executors.newCachedThreadPool();
 
     public void setPriceRetriever(PriceRetriever priceRetriever) {
         this.priceRetriever = priceRetriever;
@@ -18,7 +25,33 @@ public class PriceAggregator {
     }
 
     public double getMinPrice(long itemId) {
-        // place for your code
-        return 0;
+        List<CompletableFuture<Double>> tasks =
+            shopIds.stream()
+                .map(shopId ->
+                    CompletableFuture
+                            .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId) , executor)
+                            .exceptionally(e -> Double.NaN)
+                )
+                .toList();
+
+        CompletableFuture<Void> allTasks =
+                CompletableFuture.allOf(tasks.toArray(CompletableFuture[]::new))
+                        .orTimeout(2900, TimeUnit.MILLISECONDS);
+
+        return allTasks.handle((t, ex) ->
+            tasks.stream()
+                .filter(CompletableFuture::isDone)
+                .map(f -> {
+                            try {
+                                return f.join();
+                            } catch (Exception e) {
+                                return Double.NaN;
+                            }
+                        }
+                )
+                .filter(p -> !Double.isNaN(p) && !p.equals(0D))
+                .reduce(Double::min)
+                .orElse(Double.NaN)
+        ).join();
     }
 }
